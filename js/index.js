d48d77a1df32308685407a874a08b396dbc2b4d9
Alesanco <alessandro.innocenzi.83@gmail.com>
2018-02-06 18:34:16 +0100

    First changes.

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(window).load(function() {
+  $('#wrapper').addClass('loaded');
+});
+
+$('.more-info').click(function( ){
+  $("#card").toggleClass('flip');
+  $('#arrow').remove();
+});
+
+$('#background').click(function() {
+  $('#card').removeClass('flip');
+});
