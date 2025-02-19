50516c8a8b9bcf478fa33e79a2ec1fc2b4f1c3c2
Alessandro Innocenzi <alessandro.innocenzi.83@gmail.com>
2025-02-19 09:40:07 +0100

    Code indentation fixed.

--- base64.c
+++ base64.c
@@ -43,13 +43,13 @@ void base64_decode(char *base64, char *text) {
 }
 
 void encodeblock(unsigned char in[], char base64[], int len) {
-    unsigned char out[5];
-    out[0] = b64[ in[0] >> 2 ];
-    out[1] = b64[ ((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4) ];
-    out[2] = (unsigned char) (len > 1 ? b64[ ((in[1] & 0x0f) << 2) | ((in[2] & 0xc0) >> 6) ] : '=');
-    out[3] = (unsigned char) (len > 2 ? b64[ in[2] & 0x3f ] : '=');
-    out[4] = '\0';
-    strncat(base64, out, sizeof(out));
+  unsigned char out[5];
+  out[0] = b64[ in[0] >> 2 ];
+  out[1] = b64[ ((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4) ];
+  out[2] = (unsigned char) (len > 1 ? b64[ ((in[1] & 0x0f) << 2) | ((in[2] & 0xc0) >> 6) ] : '=');
+  out[3] = (unsigned char) (len > 2 ? b64[ in[2] & 0x3f ] : '=');
+  out[4] = '\0';
+  strncat(base64, out, sizeof(out));
 }
 
 void base64_encode(char *text, char *base64) {
@@ -61,8 +61,8 @@ void base64_encode(char *text, char *base64) {
   while(text[j]) {
     len = 0;
     for (i = 0; i < 3; i++) {
-     in[i] = (unsigned char) text[j];
-     if (text[j]) {
+      in[i] = (unsigned char) text[j];
+      if (text[j]) {
         len++; j++;
       }
       else in[i] = 0;
